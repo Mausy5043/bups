8a2b461c7e9df046de043301e80dab8b721e07ad
Maurice (mausy5043) Hendrix <Mausy5043@users.noreply.github.com>
2022-12-28 15:42:15 +0100

    no filename needed

--- bin/constants.sh
+++ bin/constants.sh
@@ -74,7 +74,7 @@ stop_bups() {
     action_services stop
     # sync the database into the cloud
     if command -v rclone; then
-        rclone sync -v "${db_full_path}" "${database_remote_root}/${app_name}/${database_filename}"
+        rclone sync -v "$${database_local_root}/${app_name}" "${database_remote_root}/${app_name}"
     fi
 }
 
